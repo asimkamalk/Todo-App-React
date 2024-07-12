5649fcffe39aa5b3e1c2351988fe796ac5ea4d1a
Asim Kamal <asimkamalk@gmail.com>
2024-07-12 12:28:17 +0500

    Basic code

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import TodoList from "./TodoList";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <TodoList />
     </>
   );
 }
---
+++ src/TodoList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function TodoList() {
+  return (
+    <>
+      <div>
+        <h2>Todo App</h2>
+        <input type="text" placeholder="Add a task" />
+        <button>Add Tak</button>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h4>Tasks Todo</h4>
+        <ul></ul>
+      </div>
+    </>
+  );
+}
